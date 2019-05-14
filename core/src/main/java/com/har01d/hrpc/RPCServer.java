629c7bac9a0c974d8d19265e7dd7994382f9c886
Harold <power721@163.com>
2019-05-14 16:10:25 +0800

    scanService

--- core/src/main/java/com/har01d/hrpc/RPCServer.java
+++ core/src/main/java/com/har01d/hrpc/RPCServer.java
@@ -1,9 +1,15 @@
 package com.har01d.hrpc;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
 public class RPCServer {
     private static final int DEFAULT_PORT = 9807;
     private int port = DEFAULT_PORT;
     private boolean isRunning = false;
+    private Map<String, Class<?>> registration = new HashMap<String, Class<?>>();
 
     public RPCServer() {
     }
@@ -12,16 +18,81 @@ public class RPCServer {
         this.port = port;
     }
 
-    public void start() {
-
+    public void start() throws IOException {
+        scanService();
     }
 
-    private void scanService() {
+    private void scanService() throws IOException {
+        List<Class<?>> classes = scanClasses();
+        Set<Class<?>> services = new HashSet<Class<?>>();
 
+        for (Class<?> clazz : classes) {
+            System.out.println(clazz.getName());
+            RPC rpc = clazz.getAnnotation(RPC.class);
+            if (rpc != null) {
+                services.add(clazz);
+            }
+        }
+
+        for (Class<?> clazz : classes) {
+            if (clazz.isInterface()) {
+                continue;
+            }
+
+            Class<?>[] interfaces = clazz.getInterfaces();
+            for (Class<?> intf : interfaces) {
+                if (services.contains(intf)) {
+                    registration.put(intf.getName(), clazz);
+                }
+            }
+        }
     }
 
-    private void registerService() {
+    private List<Class<?>> scanClasses() throws IOException {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        if (cl == null) {
+            cl = this.getClass().getClassLoader();
+        }
 
+        List<Class<?>> classes = new ArrayList<Class<?>>();
+        Enumeration<URL> enumeration = cl.getResources("");
+        while (enumeration.hasMoreElements()) {
+            URL url = enumeration.nextElement();
+            File root = new File(url.getPath());
+            scanClasses(root, root, classes);
+        }
+
+        return classes;
+    }
+
+    private void scanClasses(File root, File directory, List<Class<?>> classes) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    scanClasses(root, file, classes);
+                } else {
+                    if (file.getName().endsWith(".class")) {
+                        String path = file.getAbsolutePath().replace(".class", "");
+                        path = path.substring(root.getAbsolutePath().length());
+                        if (path.startsWith("/")) {
+                            path = path.substring(1);
+                        }
+
+                        String className = path.replace(File.separator, ".");
+                        try {
+                            Class<?> clazz = Class.forName(className);
+                            classes.add(clazz);
+                        } catch (ClassNotFoundException e) {
+                            // ignore
+                        }
+                    } else if (file.getName().endsWith(".jar")) {
+                        // TODO:
+                        System.err.println(file);
+                    }
+                }
+            }
+        }
     }
 
     public void run() {
--- example/server/src/main/java/com/har01d/example/Main.java
+++ example/server/src/main/java/com/har01d/example/Main.java
@@ -1,7 +1,12 @@
 package com.har01d.example;
 
-public class Main {
-    public static void main(String[] args) {
+import com.har01d.hrpc.RPCServer;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        RPCServer server = new RPCServer();
+        server.start();
     }
 }
